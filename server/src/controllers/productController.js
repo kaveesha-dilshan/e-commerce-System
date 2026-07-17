c0222a9303267ae5cc38122f243f5499c39260d5
kaveesha-dilshan <kaveeshadilshan5445@gmail.com>
2026-07-17 23:20:28 +0530

    Add update product, delete product, get product by ID, add pagination and small search part to productController

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -40,9 +40,32 @@ export const createProduct = async (req, res) => {
 // create get all products controller
 export const getProducts = async (req, res) => {
     try {
-        const products = await Product.find()
-            .populate("category")
-            .sort({ createdAt: -1 });
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        let products;
+        const keyword = req.query.keyword;
+        if (keyword) {
+            products = await Product.find({
+                name: {
+                    $regex: keyword,
+                    $options : "i"
+                }
+            })
+            .skip(skip)
+            .limit(limit)
+            .populate("category", "name")
+            .sort({ createdAt: -1});
+            console.log(products);
+        }else{
+            products = await Product.find()
+                .skip(skip)
+                .limit(limit)
+                .populate("category", "name")
+                .sort({ createdAt: -1});
+                console.log(products);
+        }
         res.status(200).json({
             count: products.length,
             products
@@ -54,4 +77,78 @@ export const getProducts = async (req, res) => {
     }
 }
 
-// 
+// create get product by id
+export const getProductById = async (req, res) =>{
+    try {
+        const product = await Product.findById(req.params.id);
+        
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found",
+            })
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
+}
+
+// create update product by id 
+export const updateProduct = async (req, res) => {
+    try {
+        const { name, description, price, stock } = req.body;
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found",
+            })
+        }
+        product.name = name || product.name;
+        product.description = description || product.description;
+        product.price = price || product.price;
+        product.stock = 0 || product.stock;
+
+        await product.save();
+
+        res.status(200).json({
+            message: "Product update successfully",
+            product: {
+                id: product.id,
+                name: product.name,
+                description: product.description,
+                price: product.price,
+                stock: product.stock
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
+}
+
+// create delete product by Id
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found",
+            })
+        }
+
+        await product.deleteOne();
+
+        res.status(200).json({
+            message: "Product delete successfully.",
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
+}
--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { protect, authorize } from "../middleware/authMiddleware.js";
-import { createProduct } from "../controllers/productController.js";
+import { createProduct, getProducts, getProductById, updateProduct, deleteProduct } from "../controllers/productController.js";
+import { updateCategory } from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.post("/create", protect, authorize("admin"), createProduct);
+router.get("/getallproducts", protect, getProducts);
+router.get("/:id", protect, getProductById);
+router.put("/:id", protect, authorize("admin"), updateProduct);
+router.delete("/:id", protect, authorize("admin"), deleteProduct);
 
 export default router;
